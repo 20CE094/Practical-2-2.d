ef54bbc93c143dc1e3d4bde2f4bc605fd18469ee
20CE094 <97758311+20CE094@users.noreply.github.com>
2022-01-15 00:46:35 +0530

    Add files via upload

---
+++ pratical2_2_d.py
@@ -0,0 +1,9 @@
+#Name-Nehal Patel
+#ID- 20CE094
+#Github link: https://github.com/20CE094/Practical-2-2.d.git
+#Aim: Write a Python program to convert a tuple to a string.
+t=('a','b')
+print(t)
+#converting tuple to string
+str = ''.join(t)
+print(str)
